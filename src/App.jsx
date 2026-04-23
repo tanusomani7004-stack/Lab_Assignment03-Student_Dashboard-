9488aa41f19f2b69c717254768cd71fb51dba7b3
tanusomani7004-stack <tanusomani7004@gmail.com>
2026-04-23 14:25:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,29 +7,27 @@ import AllDetails from './Components/AllDetails'
 function App() {
 
   const initialStudents = [
-    {id:1,name:"Tannu",score:97},
-    {id:2,name:"Prithvi",score:90},
-    {id:3,name:"Katreena",score:26},
-    {id:4,name:"Saket",score:12}   // this is an object 
+    {id:1,name:"Tannu",score:100},
+    {id:2,name:"Prithvi",score:99.5},
+    {id:3,name:"Divyanshi",score:26},
+    {id:4,name:"Aarti",score:11}
   ]
 
-  const [students,setStudents] = useState(initialStudents) // students ki intial walue initialStudentswali hogi
+  const [students,setStudents] = useState(initialStudents)
 
   const updateScore = (index,newScore)=>{
-    const updated = students.map(s=>s.id === index+1 ? {...s,score:Number(newScore)}:s)
+    const updated = students.map((s,i)=>
+      i === index ? {...s, score:Number(newScore)} : s
+    )
 
     setStudents(updated)
   }
-
-
- 
-
 
   return (
     <div className='w-full flex flex-col items-center gap-10 p-20 h-full bg-gray-900'>
     <Heading/>
     <AddStudent students={students} setStudents={setStudents}/>
-    <StudentsSummary/>
+    <StudentsSummary students={students}/>   {/* 🔥 important */}
     <AllDetails students={students} updateScore={updateScore}/>
     </div>
   )
